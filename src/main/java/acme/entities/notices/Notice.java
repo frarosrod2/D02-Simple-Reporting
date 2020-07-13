3df529f117aa5ce1371917a20903f996daafc296
frarosrod2 <frarosrod2@alum.us.es>
2020-07-13 18:22:51 +0200

    Inquiry, notice and overture entities created.

---
+++ src/main/java/acme/entities/notices/Notice.java
@@ -0,0 +1,53 @@
+
+package acme.entities.notices;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Future;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.entities.DomainEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Notice extends DomainEntity {
+
+	// Serialization identifier -----------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@NotBlank
+	private String				picture;
+
+	@NotBlank
+	private String				title;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Past
+	private Date				creation;
+
+	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
+	@Future
+	private Date				deadline;
+
+	@NotBlank
+	private String				body;
+
+	@URL
+	private List<String>		optionalLinks;
+
+}
